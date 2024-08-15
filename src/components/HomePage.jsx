182363dc0864a2d580d2d77b40a3ed5f6593bdfc
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-08-15 18:47:55 +0100

    feat: add icon to btn
    add header section

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 
 import Modal from './UnderDevelopment';
 import SelectProfile from './SelectProfile';
+import CreateIconSrc from '../../public/close.svg';
+import DownloadsIconSrc from '../../public/square-edit-outline.svg';
+import '../styles/HomePage.css';
 
 function HomePage() {
   const [underDevModalOpen, setUnderDevModalOpen] = useState(false);
@@ -24,27 +27,34 @@ function HomePage() {
   }
 
   return (
-    <div className='home-page'>
-      <div className='btn-group d-flex__row gap_2r'>
+    <div className='home-page d-flex__col align-items_-center justify-content__center gap_2r'>
+      <h1 className='text-transform__capitalize text-align__center'>
+        Intelligent CV
+      </h1>
+      <div className='btn-group d-flex__row gap_2r justify-content__center align-items__center'>
         <button
           type='button'
-          className='btn'
+          className='btn d-flex__row gap_1r'
           onClick={() => {
             setSelectProfileOpen(true);
           }}
         >
-          <span className='icon-container'></span>
-          <span>Create</span>
+          <span className='icon-container'>
+            <img src={CreateIconSrc} alt='' />
+          </span>
+          <span className='icon-text'>Create</span>
         </button>
         <button
           type='button'
-          className='btn'
+          className='btn d-flex__row gap_1r'
           onClick={() => {
             toggleDialog();
           }}
         >
-          <span className='icon-container'></span>
-          <span>Downloads</span>
+          <span className='icon-container'>
+            <img src={DownloadsIconSrc} alt='' />
+          </span>
+          <span className='icon-text'>Downloads</span>
         </button>
       </div>
       <Modal isOpen={underDevModalOpen} onClose={toggleDialog}>
