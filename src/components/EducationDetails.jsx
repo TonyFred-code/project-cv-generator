b7d8664d4e35fce327d6db33773aec7cea70e704
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-08-25 14:40:49 +0100

    feat: complete education details section
    feat: add deleting and creating education experiences
    feat: add updating education experiences

---
+++ src/components/EducationDetails.jsx
@@ -0,0 +1,308 @@
+import { useState } from 'react';
+// import '../styles/EducationDetails.css';
+import Icon from '@mdi/react';
+import {
+  mdiArrowLeftThin,
+  mdiCheckBold,
+  mdiPen,
+  mdiPlus,
+  mdiTrashCan,
+} from '@mdi/js';
+
+import dateFormat from 'dateformat';
+import { subYears } from 'date-fns';
+
+function EducationDetails({
+  education_details,
+  onClose,
+  onUpdateEducationDetails,
+  onEducationCreate,
+  onEducationDelete,
+}) {
+  const [activeTabId, setActiveTabId] = useState(1);
+  const [activeEducationId, setActiveEducationId] = useState(null);
+  const [stillLearning, setStillLearning] = useState(false);
+
+  function handleFormSubmit(e, experienceId) {
+    e.preventDefault();
+
+    const { elements } = e.target;
+    const [education_detail] = education_details.filter(
+      (exp) => exp.id === experienceId,
+    );
+
+    const newEducationDetails = {
+      ...education_detail,
+      date_started: elements.date_started.value,
+      study_title: elements.study_title.value,
+      school_name: elements.school_name.value,
+      still_in_study: elements.in_study.checked,
+      date_ended: `${
+        elements.in_study.checked ? '' : elements.date_ended.value
+      }`,
+    };
+
+    setActiveEducationId(null);
+    onUpdateEducationDetails(newEducationDetails);
+  }
+
+  function handleDeleteExperience(experienceId) {
+    if (activeEducationId === experienceId) {
+      setActiveEducationId(null);
+    }
+    onEducationDelete(experienceId);
+  }
+
+  console.log(education_details);
+
+  return (
+    <div className='personal-details padding_2r'>
+      <header className='d-flex__row align-items__center'>
+        <button
+          type='button'
+          className='btn btn-icon'
+          onClick={() => {
+            onClose();
+          }}
+        >
+          <span className='icon-container'>
+            <Icon path={mdiArrowLeftThin} size={3} />
+          </span>
+          <span className='icon-text'>Back</span>
+        </button>
+        <h1 className='text-transform__capitalize margin_lr_centering'>Work</h1>
+        {activeEducationId === null && activeTabId === 1 && (
+          <button
+            type='button'
+            className='btn btn-icon'
+            onClick={() => {
+              setStillLearning(false);
+              setActiveEducationId(onEducationCreate());
+            }}
+          >
+            <span className='icon-container'>
+              <Icon path={mdiPlus} size={3} />
+            </span>
+            <span className='icon-text'>Add</span>
+          </button>
+        )}
+      </header>
+
+      <div className='d-flex__col gap_1r'>
+        <div className='tab-area d-flex__row align-items__center justify-content__space-around padding_1r'>
+          <button
+            className={`personal_details_view_tab tab-btn btn ${
+              activeTabId === 1 && 'active'
+            }`}
+            onClick={() => {
+              setActiveTabId(1);
+            }}
+          >
+            <span>Education Experience</span>
+          </button>
+          <button
+            className={`help_view_tab tab-btn btn ${
+              activeTabId === 2 && 'active'
+            }`}
+            onClick={() => {
+              [setActiveTabId(2)];
+            }}
+          >
+            <span>Help</span>
+          </button>
+        </div>
+
+        <div className='active-tab-view'>
+          {activeTabId === 1 ? (
+            <>
+              <header>
+                <h1 className='text-align__center'>Education Experiences</h1>
+              </header>
+              <div className='d-flex__col gap_2r padding_1r'>
+                {education_details.map((education_detail, index) => {
+                  const {
+                    school_name,
+                    study_title,
+                    date_started,
+                    date_ended,
+                    still_in_study,
+                    id,
+                  } = education_detail;
+
+                  return (
+                    <div className='experience-card' key={id}>
+                      <header className='d-flex__row align-items__center'>
+                        {activeEducationId === id && (
+                          <button
+                            type='submit'
+                            form={`education_details_form_${id}`}
+                            className='btn btn-icon'
+                          >
+                            <span className='icon-container'>
+                              <Icon path={mdiCheckBold} size={3} />
+                            </span>
+                            <span className='icon-text'>Save</span>
+                          </button>
+                        )}
+                        <h2 className='text-transform__capitalize margin_lr_centering'>
+                          Experience {index + 1}
+                        </h2>
+                        <div className='d-flex__row align-items_center gap_1r'>
+                          {activeEducationId === null && (
+                            <button
+                              type='button'
+                              className='btn btn-icon'
+                              onClick={() => {
+                                setActiveEducationId(id);
+                              }}
+                            >
+                              <span className='icon-container'>
+                                <Icon path={mdiPen} />
+                              </span>
+                              <span className='icon-text'>Edit</span>
+                            </button>
+                          )}
+                          <button
+                            type='button'
+                            className='btn d-flex__row align-items__center gap_1r'
+                            onClick={() => {
+                              handleDeleteExperience(id);
+                            }}
+                            title='Delete Profile'
+                          >
+                            <span className='icon-container'>
+                              <Icon path={mdiTrashCan} size={2} />
+                            </span>
+                          </button>
+                        </div>
+                      </header>
+
+                      {activeEducationId === id ? (
+                        <form
+                          id={`education_details_form_${id}`}
+                          autoComplete='true'
+                          autoCorrect='true'
+                          className='d-flex__col gap_2r experience-card-edit'
+                          onSubmit={(e) => {
+                            handleFormSubmit(e, id);
+                          }}
+                        >
+                          <div className='form-row d-flex__col gap_1r'>
+                            <label htmlFor='school_name'>School Name</label>
+                            <input
+                              required
+                              type='text'
+                              defaultValue={school_name}
+                              name='school_name'
+                              id='school_name'
+                            />
+                          </div>
+
+                          <div className='form-row d-flex__col gap_1r'>
+                            <label htmlFor='study_title'>Title of Study</label>
+                            <input
+                              required
+                              type='text'
+                              defaultValue={study_title}
+                              name='study_title'
+                              id='study_title'
+                            />
+                          </div>
+
+                          <div className='form-row d-flex__col gap_1r'>
+                            <label htmlFor='date_started'>Started Study</label>
+                            <input
+                              type='month'
+                              name='date_started'
+                              id='date_started'
+                              defaultValue={date_started}
+                              max={dateFormat(new Date().now, 'yyyy-mm')}
+                              min={dateFormat(
+                                subYears(new Date(), 10),
+                                'yyyy-mm',
+                              )}
+                            />
+                          </div>
+
+                          <div className='form-row d-flex__col gap_1r'>
+                            <div className='d-flex__row justify-content__space-between'>
+                              <label htmlFor='date_ended'>Study Ended</label>
+
+                              <span>
+                                <label
+                                  htmlFor='in_study'
+                                  className='d-flex__row align-items__center gap_1r'
+                                >
+                                  <input
+                                    type='checkbox'
+                                    name='in_study'
+                                    id='in_study'
+                                    checked={stillLearning}
+                                    onChange={() => {
+                                      setStillLearning(!stillLearning);
+                                    }}
+                                  />
+                                  Till Present
+                                </label>
+                              </span>
+                            </div>
+                            {!stillLearning && (
+                              <input
+                                type='month'
+                                name='date_ended'
+                                id='date_ended'
+                                max={dateFormat(new Date().now, 'yyyy-mm')}
+                                min={dateFormat(
+                                  subYears(new Date(), 10),
+                                  'yyyy-mm',
+                                )}
+                                defaultValue={date_ended}
+                              />
+                            )}
+                          </div>
+                        </form>
+                      ) : (
+                        <div>
+                          <div className='detail d-flex__col gap_1r padding_1r'>
+                            <p className='light-text'>School Name</p>
+                            <p className='padding-left_1r'>{school_name}</p>
+                          </div>
+                          <div className='detail d-flex__col gap_1r padding_1r'>
+                            <p className='light-text'>title of study</p>
+                            <p className='padding-left_1r'>{study_title}</p>
+                          </div>
+                          {date_started !== '' && (
+                            <div className='detail d-flex__col gap_1r padding_1r'>
+                              <p className='light-text'>Started Studying</p>
+                              <p className='padding-left_1r'>
+                                {dateFormat(date_started, 'mmmm, yyyy')}
+                              </p>
+                            </div>
+                          )}
+                          {(date_ended !== '' || still_in_study) && (
+                            <div className='detail d-flex__col gap_1r padding_1r'>
+                              <p className='light-text'>Finished Studying</p>
+                              <p className='padding-left_1r'>
+                                {still_in_study
+                                  ? 'Till Present'
+                                  : dateFormat(date_ended, 'mmmm, yyyy')}
+                              </p>
+                            </div>
+                          )}
+                        </div>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            </>
+          ) : (
+            <div> Education Experience Help</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EducationDetails;
