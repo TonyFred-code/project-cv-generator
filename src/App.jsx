7d30bb97ad7adcf4ad85750d9baeb869f26d0235
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-08-30 23:45:31 +0100

    feat: add loading comp import
    switch from loading to app with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import './styles/Base.css';
 import HomePage from './components/HomePage';
+import Loader from './components/Loader';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  if (isLoading) {
+    return (
+      <Loader
+        onLoadComplete={() => {
+          setIsLoading(false);
+        }}
+      />
+    );
+  }
+
   return <HomePage />;
 }
 
